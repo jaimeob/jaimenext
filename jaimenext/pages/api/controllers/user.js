50f2d859e723bc09fe86de34aebc592ccb20c413
Jaime Ojeda Buelna <jaime.ojeda@coppel.com>
2022-11-28 14:54:29 -0700

    subir

---
+++ jaimenext/pages/api/controllers/user.js
@@ -0,0 +1,3 @@
+exports.postData() = () =>{
+res.send()
+}
---
+++ jaimenext/server.js
@@ -0,0 +1,9 @@
+const express = require("express")
+
+const app = express()
+
+const port = 3000
+
+
+// const userRouter = require(..pages)
+// app.use()
