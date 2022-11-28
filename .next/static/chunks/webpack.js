01c749ff86c4d508e0039eb5c5e30a90fd7435c5
Jaime Ojeda Buelna <jaime.ojeda@coppel.com>
2022-11-28 14:44:54 -0700

    subiendo pa

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b468303b59693fa2"; }
+/******/ 		__webpack_require__.h = function() { return "54d208ddeb2ba423"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
